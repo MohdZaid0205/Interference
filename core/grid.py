52cce0a2ec4f01c088ec736b578dd815f0eb1be0
Mohd Zaid <mdzaid0205@gmail.com>
2024-12-10 18:36:23 +0530

    fix `modifier` should modify value before storing.

--- core/grid.py
+++ core/grid.py
@@ -9,9 +9,9 @@ class Grid[Member]:
         self.n = n
         self.__grid = [[default for _ in range(n)] for _ in range(m)]
 
-    def set[T](self, i: int, j: int, value: Optional[Member] = None, modifier: Optional[Callable[[Member],T]] = None) -> Optional[T]:
+    def set[T](self, i: int, j: int, value: Optional[Member] = None, modifier: Optional[Callable[[Member],T]] = None) -> None:
         if modifier is not None:
-            return modifier(self.__grid[i][j])
+            self.__grid[i][j] = modifier(value)
         self.__grid[i][j] = value
 
     def get[T](self, i: int, j: int, specifier: Optional[Callable[[Member],T]] = None) -> Union[T, Member]:
