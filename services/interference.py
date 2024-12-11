bebdcf23a521e34f631fa0e61dad681830b3be47
Mohd Zaid <mdzaid0205@gmail.com>
2024-12-11 18:15:43 +0530

    add clas to manage and calculate intereference

---
+++ services/interference.py
@@ -0,0 +1,26 @@
+from core.grid import Grid
+from core.particle import Particle
+from core.wave import WaveEquation, WaveSourceEmitter
+
+from typing import Callable, Generator
+
+
+class Interference:
+
+    def __init__(self, grid:Grid[Particle], sources:list[WaveSourceEmitter]) -> None:
+        self.grid = grid
+        self.sorc = sources
+
+    def __next__[T](self, provider:Callable[[Particle], T]) -> Generator[T, None, None]:
+        for i in range(self.grid.m):
+            for j in range(self.grid.n):
+                yield (i, j, self.grid.get(i, j, provider))
+    
+    def update(self, time:float) -> None:
+        for i in range(self.grid.m):
+            for j in range(self.grid.n):
+                particle = self.grid.get(i, j)
+                particle.d = 0
+                for source in self.sorc:
+                    particle.d += source.calculateDisplacement(particle.P, time)
+        ...
